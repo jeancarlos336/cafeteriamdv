de3ae3e40c3dc801febf1f808717ac1de18322d2
Juan Carlos Ortega Soto <jortega@integra.cl>
2024-11-22 23:13:39 -0300

    modifica impresion de boleta

--- pos_system/pos_system/settings.py
+++ pos_system/pos_system/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "your-default-secret-key")
 DEBUG = os.getenv("DEBUG", "True") == "True"
 
 # Hosts permitidos dinámicos
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1,3.208.20.119").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1,44.220.163.99").split(",")
 
 # Application definition
 INSTALLED_APPS = [
